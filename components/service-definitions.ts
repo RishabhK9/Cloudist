f624bacb4f6bbabb4a1b261488e91ba9362eeedf
Abhay Sudhir <abhaysudhir@icloud.com>
2025-10-25 17:47:30 -0700

    Merge: Resolve rebase conflicts

---
+++ components/service-definitions.ts
@@ -0,0 +1,5 @@
+// This file is deprecated. All service definitions are now loaded from JSON config files.
+// Use the ConfigLoader from @/lib/config-loader instead.
+
+export { ConfigLoader, type ConfigField, type ServiceConfig } from '@/lib/config-loader';
+
